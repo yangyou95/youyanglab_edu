a30d18c1bbcfd1f6c7256127c8248cc55ba11638
Yang You <y.you@cranfield.ac.uk>
2019-12-08 14:04:46 +0100

    update the django codes

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path
 from .import views
 
+# 在每个应用里，规定命名空间app_name，防止其他应用里也有同名的视图
 app_name = 'webapp'
 urlpatterns = [
-    path('', views.index, name = 'index')
+    # 改良前
+
+    # path('', views.index, name = 'index'),
+    #  # ex: /webapp/5/
+    # path('<int:question_id>/', views.detail, name='detail'),
+    # # ex: /webapp/5/results/
+    # path('<int:question_id>/results/', views.results, name='results'),
+    # # ex: /webapp/5/vote/
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+
+    #改良后
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,11 +1,74 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+# from django.template import loader
+from django.urls import reverse
+from django.views import generic
 
 # Create your views here.
 from .models import Question, Choice
 
 # 提取问题并显示在前端
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    context['variable'] = "传递参数测试"
-    return render(request, 'webapp/index.html', context)
+
+#改良前
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     context['variable'] = "传递参数测试"
+#     return render(request, 'webapp/index.html', context)
+
+#     # # 定义的模板
+#     # template = loader.get_template('webapp/index.html')
+#     # context = {
+#     #     'latest_question_list':latest_question_list,
+#     # }
+#     # return HttpResponse(template.render(context, request))
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'webapp/detail.html', {'question': question})
+
+# def results(request, question_id):
+#     # response = "You're looking at the results of question %s."
+#     # return HttpResponse(response % question_id)
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'webapp/results.html', {'question': question})
+
+#改良后
+class IndexView(generic.ListView):
+    template_name = 'webapp/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'webapp/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'webapp/results.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'webapp/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('webapp:results', args=(question.id,)))
