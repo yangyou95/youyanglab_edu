e9e0d96d5f7b77b4da3bbe44ccedebdab7b7b54e
Yang You <y.you@cranfield.ac.uk>
2019-12-12 19:36:08 +0100

    Update settings.py

--- youyang_lab/settings.py
+++ youyang_lab/settings.py
@@ -121,6 +121,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 #used for heroku, name a static root folder
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#
+# STATIC_URL = '/static/'
 
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
